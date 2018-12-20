32e57a701d3ba0e056c6e5d1ddbd8c266e4681a7
anonimD08 <asdebug@yandex.ru>
2018-12-20 17:49:29 +0500

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import '../static/css/Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap, faAppleAlt } from '@fortawesome/free-solid-svg-icons';
 
 class Header extends Component {
 
 	render(){
 	  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Урфушка лол</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Support</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
-        </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+    <div className="navbar-container">
+    <div className="nav-background"></div>
+        <nav className="navbar navbar-expand-lg navbar-light">
+        <a className="navbar-brand" href="#"><FontAwesomeIcon icon={faGraduationCap} size="1x"/> Ural Federal University</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Support</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+            </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+        </nav>
+        <div className="nav-tilte-image"><h2>Каталог онлайн-курсов</h2><FontAwesomeIcon icon={faAppleAlt} size="2x"/> </div>
     </div>
-    </nav>
     )
   }
 }
