e292fae4bc260a7ea54c9f8ada60b276bc35ac45
MasterGowen <mastergowen@gmail.com>
2019-02-06 16:27:24 +0500

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import '../static/css/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGraduationCap, faSearch } from '@fortawesome/free-solid-svg-icons';
 
+import logo from '../static/img/logo.png'
+
 class Header extends Component {
     constructor(){
         super()
@@ -44,7 +46,11 @@ class Header extends Component {
         <div className="navbar-container">
         <div className="filter-back"></div><div className="nav-background"></div>
             <nav className="navbar navbar-expand-lg navbar-light ">
-            <a className="navbar-brand" href="/"><FontAwesomeIcon icon={faGraduationCap} size="1x"/> Ural Federal University</a>
+
+            <a className="navbar-brand" href="/"><FontAwesomeIcon icon={faGraduationCap} size="1x"/>
+            <img src={logo} alt="Открытые образовательные программы">
+             Открытые образовательные программы и курсы УрФУ
+             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
