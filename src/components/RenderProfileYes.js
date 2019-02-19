20de30f77aa3b6ad23e549579f39d3ac082dedeb
anonimD08 <asdebug@yandex.ru>
2019-02-19 13:12:03 +0500

    #1

--- src/components/RenderProfileYes.js
+++ src/components/RenderProfileYes.js
@@ -8,15 +8,15 @@ import {MEDIA_LS_URL} from '../services/openurfu';
 class RenderProfileYes extends Component {
 
 	render(){
-        
+        console.log(this.props)
 	  return (
         <div>
         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
             <li className="nav-item">
-                <img src={this.props.data.profile_image} alt={this.props.data.username} />
+                {/* <img src={this.props.data.profile_image} alt={this.props.data.username} /> */}
             </li>
             <li className="nav-item">
-                <p>{this.props.data.username}</p>
+                <p>Test</p>
             </li>
         </ul>
     </div>
