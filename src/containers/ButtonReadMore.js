6366dc35e1baf4516d230d4d240fcf809755e6e7
anonimD08 <asdebug@yandex.ru>
2019-03-29 13:33:24 +0500

    Button info course

--- src/containers/ButtonReadMore.js
+++ src/containers/ButtonReadMore.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../static/css/CourseAbout.css';
 import 'animate.css/animate.min.css';
+import  { MEDIA_LS_URL } from '../services/openurfu';
 
 class ButtonReadMore extends Component {
 
 
     render(){
+      let url = `${MEDIA_LS_URL}/courses/${this.props.match.params.id}/info`
       return (
-        <button className="btn btn-primary btn-lg">Перейти к курсу</button>
+        <a href={url}>
+          <button className="btn btn-primary btn-lg">Перейти к курсу</button>
+        </a>
+        
     )
   }
 }
